c89503ccd552f9126d7f85180a576481f84466e8
PetarZecevic97 <petar.b.zecevic@gmail.com>
2021-03-09 14:57:21 +0100

    added vets list and updated controller. closes #15

--- petclinicweb/src/main/java/petar/zecevic/petclinic/controllers/VetController.java
+++ petclinicweb/src/main/java/petar/zecevic/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package petar.zecevic.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import petar.zecevic.petclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
