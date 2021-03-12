c33a9328d8691d7e6b0d96c2ccf6fb60e3c4ecf7
PetarZecevic97 <petar.b.zecevic@gmail.com>
2021-03-12 16:59:35 +0100

    added spring data jpa service for owners. closes #40

---
+++ petclinicdata/src/main/java/petar/zecevic/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,64 @@
+package petar.zecevic.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import petar.zecevic.petclinic.model.Owner;
+import petar.zecevic.petclinic.repositories.OwnerRepository;
+import petar.zecevic.petclinic.services.OwnerService;
+import petar.zecevic.petclinic.services.PetService;
+import petar.zecevic.petclinic.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetTypeService petTypeService, PetService petService) {
+        this.ownerRepository = ownerRepository;
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+
+        ownerRepository.findAll().forEach(owners::add);
+
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+        ownerRepository.deleteById(aLong);
+
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
